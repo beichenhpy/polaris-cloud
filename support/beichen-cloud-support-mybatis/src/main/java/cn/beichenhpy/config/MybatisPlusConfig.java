529f0cb6ac8eeaa840c0e1860430ef43b59766e1
beichenhpy <beichenhpy@outlook.com>
2021-06-25 17:21:09 +0800

    feat(mybatis):添加Mybatis-plus-support

---
+++ support/beichen-cloud-support-mybatis/src/main/java/cn/beichenhpy/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package cn.beichenhpy.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author beichenhpy
+ * @version 0.0.1
+ * @apiNote MybatisPlusConfig description：
+ * @since 2021/6/25 16:41
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
---
+++ support/beichen-cloud-support-mybatis/src/main/java/cn/beichenhpy/handler/FiledInjectHandler.java
@@ -0,0 +1,28 @@
+package cn.beichenhpy.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author beichenhpy
+ * @version 0.0.1
+ * @apiNote FiledInjectHandler description：字段自动填充
+ * @since 2021/6/25 16:42
+ */
+@Component
+public class FiledInjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("createTime", new Date(), metaObject);
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+}
