c280bd149e67d2db4265746a9db820d234c76edf
beichenhpy <beichenhpy@outlook.com>
2021-06-27 16:40:53 +0800

    feat(aop):add ExceptionAspect to do something afterThrowing

---
+++ support/beichen-cloud-support-common/src/main/java/cn/beichenhpy/aspect/ExceptionAspect.java
@@ -0,0 +1,39 @@
+package cn.beichenhpy.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author beichenhpy
+ * @apiNote Exception切面处理demo spring执行顺序：aspect -> controllerAdvice -> interceptor -> filter
+ * <p>因此 AfterThrowing后也会执行ControllerAdvice 可与 seata结合实现全局回滚
+ * @version 1.0.0
+ */
+@Aspect
+@Component
+@Slf4j
+public class ExceptionAspect {
+
+    @Before("execution(* cn.beichenhpy.service..*.*(..))")
+    public void before(JoinPoint joinpoint) {
+        MethodSignature signature = (MethodSignature) joinpoint.getSignature();
+        final Method method = signature.getMethod();
+        //这里可以获取xid
+        log.info("intercept method: "+method.getName());
+    }
+
+    @AfterThrowing(throwing = "e",pointcut = "execution(* cn.beichenhpy.service..*.*(..))")
+    public void afterThrow(Throwable e){
+        log.info("method call cn.beichenhpy.exception: "+e.getMessage());
+        //这里可以回滚事务
+    }
+
+}
+
