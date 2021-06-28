dc5aabb8685937030859e9f8cd59e530cb8a4c1d
beichenhpy <beichenhpy@outlook.com>
2021-06-28 14:32:04 +0800

    fix(consumer/feign) fix FeignClient 'fallback' param and requestPath

--- example/consumer/src/main/java/cn/beichenhpy/service/feign/ProviderFeignService.java
+++ example/consumer/src/main/java/cn/beichenhpy/service/feign/ProviderFeignService.java
@@ -1,10 +1,12 @@
 package cn.beichenhpy.service.feign;
 
 import cn.beichenhpy.modal.Comment;
+import cn.beichenhpy.service.feign.fallback.ProviderFeignServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -14,8 +16,8 @@ import java.util.List;
  * @apiNote
  * @since 2021/6/28 12:04
  */
-@FeignClient(name = "provider")
+@FeignClient(name = "provider",fallback = ProviderFeignServiceImpl.class)
 public interface ProviderFeignService {
-    @GetMapping("/info/{aid}")
+    @GetMapping("/api/v1/info/{aid}")
     ResponseEntity<List<Comment>> getCommentInfoByArticleId(@PathVariable("aid") String aid);
 }
