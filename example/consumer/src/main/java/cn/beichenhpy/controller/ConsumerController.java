a11a4af08ea0021294e99e4df9b90100b598b997
beichenhpy <beichenhpy@outlook.com>
2021-06-29 01:05:50 +0800

    feat(transactional):add test for @Transactional and @ControllerAdvice

--- example/consumer/src/main/java/cn/beichenhpy/controller/ConsumerController.java
+++ example/consumer/src/main/java/cn/beichenhpy/controller/ConsumerController.java
@@ -6,11 +6,13 @@ import cn.beichenhpy.service.ConsumerService;
 import cn.beichenhpy.service.feign.ProviderFeignService;
 import cn.beichenhpy.utils.asserts.AssertToolkit;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,5 +44,16 @@ public class ConsumerController {
                 .ok()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .body(article);
+    }
+
+    /**
+     * 证明，事务回滚和ControllerAdvice全局异常管理不冲突，因为事务回滚还会抛出原异常
+     */
+    @GetMapping("/testControllerAdviceAndTransactional")
+    @Transactional
+    public void testControllerAdviceAndTransactional(){
+        LambdaUpdateWrapper<Article> update = new LambdaUpdateWrapper<Article>().eq(Article::getId, "1408375341166145537").set(Article::getTitle, "update");
+        consumerService.update(update);
+        throw new IllegalArgumentException("事务测试");
     }
 }
