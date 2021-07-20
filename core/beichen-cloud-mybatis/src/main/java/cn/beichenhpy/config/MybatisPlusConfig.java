7090668a8bb2b4cfacda218aca0d2a4c70416458
beichenhpy <beichenhpy@outlook.com>
2021-07-20 21:03:35 +0800

    refactor(core):重构项目

--- core/beichen-cloud-mybatis/src/main/java/cn/beichenhpy/config/MybatisPlusConfig.java
+++ core/beichen-cloud-mybatis/src/main/java/cn/beichenhpy/config/MybatisPlusConfig.java
@@ -1,10 +1,8 @@
 package cn.beichenhpy.config;
 
-import cn.beichenhpy.handler.FiledInjectHandler;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
