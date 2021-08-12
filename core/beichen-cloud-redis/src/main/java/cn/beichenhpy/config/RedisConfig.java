743af77ab366fa90698a7480d54a4fb0e3b8236f
beichenhpy <beichenhpy@outlook.com>
2021-08-12 21:52:05 +0800

    fix(Redis):配置修改
    1.删除@ConditionalOnBean(LettuceConnectionFactory.class)
    2.改为泛型value

--- core/beichen-cloud-redis/src/main/java/cn/beichenhpy/config/RedisConfig.java
+++ core/beichen-cloud-redis/src/main/java/cn/beichenhpy/config/RedisConfig.java
@@ -27,8 +27,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    @ConditionalOnBean(LettuceConnectionFactory.class)
-    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+    public RedisTemplate<String, ?> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
         // 设置序列化
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper om = new ObjectMapper();
@@ -36,7 +35,7 @@ public class RedisConfig {
         om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
         jackson2JsonRedisSerializer.setObjectMapper(om);
         // 配置redisTemplate
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<String, Object>();
+        RedisTemplate<String, ?> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         RedisSerializer<?> stringSerializer = new StringRedisSerializer();
         redisTemplate.setKeySerializer(stringSerializer);
